9e89d41088bc6e16d198b537445611febffdc6c9
molodoi <mr_stat12@hotmail.com>
2023-05-17 15:58:05 +0200

    Login Register ResetForgetPass Auth Layouts Tasks

---
+++ src/layouts/AuthLayout.jsx
@@ -0,0 +1,55 @@
+import { Navigate, Outlet, Link } from "react-router-dom";
+import useAuthContext from "../context/AuthContext";
+
+const AuthLayout = () => {
+    const { user, logout } = useAuthContext();
+
+    return user ? (
+        <>
+            <nav className="bg-sky-800 text-white px-2 py-2.5 sm:px-4">
+                <div className="container mx-auto flex flex-wrap items-center justify-between">
+                    <span className="p-2 text-white">Welcome</span>
+
+                    <ul className="flex md:mt-0">
+                        {user ? (
+                            <li>
+                                <button
+                                    onClick={logout}
+                                    className="p-2 text-white"
+                                >
+                                    Logout
+                                </button>
+                            </li>
+                        ) : (
+                            <>
+                                <li>
+                                    <Link
+                                        to="/login"
+                                        className="p-2 text-white"
+                                        aria-current="page"
+                                    >
+                                        Login
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        to="/register"
+                                        className="p-2 text-white"
+                                        aria-current="page"
+                                    >
+                                        Register
+                                    </Link>
+                                </li>
+                            </>
+                        )}
+                    </ul>
+                </div>
+            </nav>
+            <Outlet />
+        </>
+    ) : (
+        <Navigate to="/login" />
+    );
+};
+
+export default AuthLayout;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,76 @@
+import { useEffect, useState } from "react";
+import axios from "../api/axios";
+import useAuthContext from "../context/AuthContext";
+const Home = () => {
+    const { user } = useAuthContext();
+    const [tasks, setTasks] = useState([]);
+
+    const getTasks = async () => {
+        const { data } = await axios.get("/api/v1/tasks");
+        setTasks(data);
+    };
+
+    useEffect(() => {
+        getTasks();
+    }, []);
+
+    return (
+        <div className="container mx-auto mt-12">
+            <section className="text-gray-600 body-font">
+                <div className="container px-5 py-24 mx-auto">
+                    <div className="flex flex-col text-center w-full mb-20">
+                        <h1 className="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">
+                            Tasks
+                        </h1>
+                        <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+                            Make the tasks come through
+                        </p>
+                    </div>
+                    <div className="lg:w-2/3 w-full mx-auto overflow-auto">
+                        <table className="table-auto w-full text-left whitespace-no-wrap">
+                            <thead>
+                                <tr>
+                                    <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">
+                                        Name
+                                    </th>
+                                    <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                                        Description
+                                    </th>
+                                    <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                                        Status
+                                    </th>
+                                    <th className="w-10 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {tasks.data?.map((task) => (
+                                    <tr key={task.id}>
+                                        <td className="px-4 py-3">
+                                            {task.title}
+                                        </td>
+                                        <td className="px-4 py-3">
+                                            {task.description}
+                                        </td>
+                                        <td className="px-4 py-3">
+                                            {task.status}
+                                        </td>
+                                        <td className="w-10 text-center">
+                                            <input name="plan" type="radio" />
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                    <div className="flex pl-4 mt-4 lg:w-2/3 w-full mx-auto">
+                        <button className="flex ml-auto text-white bg-sky-800 border-0 py-2 px-6 focus:outline-none hover:bg-sky-900 rounded">
+                            Button
+                        </button>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default Home;
